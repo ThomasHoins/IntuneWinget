abffa8bec5b6d455ee06b8e6f5eae240149b6d12
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2023-08-18 12:59:37 +0200

    update check

--- winget-program-template/check.ps1
+++ winget-program-template/check.ps1
@@ -3,21 +3,18 @@ Param (
 	)
 
 $ProgramFiles = "$Env:Programfiles"
-Start-Transcript -Path "$ProgramFiles\_MEM\Log\$ProgramName-install.log" -Force -Append
+$Nix=Start-Transcript -Path "$ProgramFiles\_MEM\Log\$ProgramName-install.log" -Force -Append
 
 # resolve winget_exe
 $WingetPath =  (Resolve-Path "$ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe")[-1].path
 
 
 If ($WingetPath){
-    $ProcessResult = Start-Process -wait -NoNewWindow -FilePath "$($WingetPath)\winget.exe" -ArgumentList "list --exact --id $ProgramName  --accept-source-agreements"
-        if ($ProcessResult -like "*$ProgramName*"){
-			Write-Host "$ProgramName is installed."
-		}
-
+    $ProcessResult =(&"$($WingetPath)\winget.exe" list --exact --id $ProgramName  --accept-source-agreements)
+	If(($ProcessResult)[-1] -like "*$ProgramName*"){Write-Host "$ProgramName is installed"}
 	}
 Else {
 	Write-Error "Winget not installed"
 	}
 
-Stop-Transcript
+$Nix=Stop-Transcript
